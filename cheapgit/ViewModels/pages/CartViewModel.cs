4777e4434a697d1393bfc7c24ccbd2d4d2cb439d
Mathew McPherson <Matmcpherson88@gmail.com>
2020-05-01 12:47:47 +0100

    TRY-MM-70 Make the Cart View Model 	[*] cheapgit.DAL/Models/OrderItem.cs 	[*] cheapgit/ViewModels/pages/CartViewModel.cs

---
+++ cheapgit/ViewModels/pages/CartViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.Models;
+using cheapgit.DAL.Models;
+
+namespace cheapgit.ViewModels.pages
+{
+    public class CartViewModel : ContentModel
+    {
+        public CartViewModel(IPublishedContent content) 
+            : base(content)
+        {
+
+        }
+
+        public IEnumerable<OrderItem> Items { get; set; }
+
+        public float TotalValue
+        {
+            get
+            {
+                return TotalValue;
+            }
+            private set
+            {
+                TotalValue = (float) Items.Sum(m => m.product.price * (float)m.quantity);
+            }
+        }
+    }
+}
